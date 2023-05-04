81a9f5745f8cc255c17a6fbd72091ebd003d9c20
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-05-04 11:35:51 -0400

    📦 NEW: able to grab a full list of users and mark if they are friends already or not

--- backend/resources/friends.py
+++ backend/resources/friends.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
 from flask_restful import Resource
-from database.models import db, Friend
+from database.models import db, Friend, User
 from database.schemas import friend_schema, friends_schema
 
 class Friend(Resource):
@@ -22,7 +22,28 @@ class Friend(Resource):
         return friend_schema.dump(add_friend), 201
 
     @jwt_required()
-    def post(self):
+    def get(self):
         user_id = get_jwt_identity()
         friends = Friend.query.get_or_404(user_id=user_id)
         return friends_schema.dump(friends), 200
+
+class FindFriends(Resource):
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+        friends = Friend.query.filer_by(user_id = user_id)
+        users = User.query.all()
+        response = []
+        for user in users:
+            already_friends = False
+            for friend in friends:
+                if(user.id == user_id or friend.friend_id == user.id):
+                    already_friends = True
+            response.append({
+                'user_id': user.id,
+                'username': user.username,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'already_friends': already_friends
+            })
+        return response, 200
